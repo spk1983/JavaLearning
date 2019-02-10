905203e7693aabc343a779eca76f9fee25258a48
skothalkar <skothalkar@AIPL-SEZ-LT-188.altimetrik.com>
2019-02-10 20:36:22 +0530

    Revert "Logic Applied."
    This reverts commit 3dcefc8d2baa16cde1b721e9a16b027f6d9795bc.

--- stock-analysis/src/fileprocessors/StockFileData.java
+++ stock-analysis/src/fileprocessors/StockFileData.java
@@ -9,10 +9,7 @@ public class StockFileData {
 	List<HashMap<String, Double>> data = new LinkedList<>();
 	
 	public void printData(){
-		for(int i=0; i<data.size(); i++) {
-			System.out.println(data.get(i));
-		}
-		
+		System.out.println(data);
 	}
 	
 	public void addData(List<HashMap<String, Double>> dataIn){
